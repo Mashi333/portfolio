8cb701a49af39770ee35b8c9851f0a02b302035c
Mashi333 <166200999+Mashi333@users.noreply.github.com>
2025-04-24 17:27:29 +0530

    About Section

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var typed = new typed(".text", {
+var typed = new Typed(".text", {
   strings: ["Developer", "Data Analyst", "Freelancer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
 });
-/*missing*/
